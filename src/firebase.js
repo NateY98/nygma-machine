16694c5b2c0f7b7751af19857b54010aa3bb81ed
NateY98 <nate.yeow@gmail.com>
2021-06-21 13:58:38 -0400

    fix firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,14 @@
 import firebase from "firebase/app";
 import "firebase/database";
 // Your web app's Firebase configuration
-var firebaseConfig = {
-  apiKey: "AIzaSyDGGGb95TwGTY8m5p1jAfSmAOZ5Y1iN3SE",
-  authDomain: "juno-zoltan.firebaseapp.com",
-  projectId: "juno-zoltan",
-  storageBucket: "juno-zoltan.appspot.com",
-  messagingSenderId: "232035626294",
-  appId: "1:232035626294:web:7e942bf960397c0bddd797",
+const firebaseConfig = {
+  apiKey: "AIzaSyCXkp_ON6ZFOAVfErbywpp3AB2MIqpdkrE",
+  authDomain: "mazeproj-1c927.firebaseapp.com",
+  databaseURL: "https://mazeproj-1c927-default-rtdb.firebaseio.com",
+  projectId: "mazeproj-1c927",
+  storageBucket: "mazeproj-1c927.appspot.com",
+  messagingSenderId: "833232080653",
+  appId: "1:833232080653:web:68a57ae85af82766afea44",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
