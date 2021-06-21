b11b38b93ec5b4565bfe0332d3393e3870b3c095
NateY98 <nate.yeow@gmail.com>
2021-06-21 15:33:54 -0400

    fixed firebase for real this time

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,12 @@ import firebase from "firebase/app";
 import "firebase/database";
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCXkp_ON6ZFOAVfErbywpp3AB2MIqpdkrE",
-  authDomain: "mazeproj-1c927.firebaseapp.com",
-  databaseURL: "https://mazeproj-1c927-default-rtdb.firebaseio.com",
-  projectId: "mazeproj-1c927",
-  storageBucket: "mazeproj-1c927.appspot.com",
-  messagingSenderId: "833232080653",
-  appId: "1:833232080653:web:68a57ae85af82766afea44",
+  apiKey: "AIzaSyBaCIzc0d5Z1CpNMsVZC7UJG7EWkgHJy_g",
+  authDomain: "zoltannat-55eaa.firebaseapp.com",
+  projectId: "zoltannat-55eaa",
+  storageBucket: "zoltannat-55eaa.appspot.com",
+  messagingSenderId: "760219817103",
+  appId: "1:760219817103:web:d1eef7a571873c20420cfa",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
